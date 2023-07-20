ab680e4c54d1475fa967fa82b340533982d2c04f
AdejohOS <elusivebrown@gmail.com>
2023-07-20 11:08:45 +0100

    initial commit

--- src/components/EditorOutput.tsx
+++ src/components/EditorOutput.tsx
@@ -1,63 +1,41 @@
 'use client'
 
+import CustomCodeRenderer from '@/components/renderers/CustomCodeRenderer'
+import CustomImageRenderer from '@/components/renderers/CustomImageRenderer'
+import { FC } from 'react'
 import dynamic from 'next/dynamic'
-import Image from 'next/image'
 
 const Output = dynamic(
-    async () => (await import ('editorjs-react-renderer')).default,
-    {
-        ssr: false
-    }
+  async () => (await import('editorjs-react-renderer')).default,
+  { ssr: false }
 )
 
 interface EditorOutputProps {
-    content: any
-}
-
-const style = {
-    paragraph: {
-        fontSize: '0.875',
-        lineHeight: '1.25rem',
-    }
+  content: any
 }
 
 const renderers = {
-    image: CustomImageRenderer,
-    code: CustomCodeRenderer,
+  image: CustomImageRenderer,
+  code: CustomCodeRenderer,
 }
 
-const EditorOutput = ({content}: EditorOutputProps) => {
-    return (
-        <Output
-            data={content}
-            className='text-sm'
-            renderers={renderers}
-            style={style}
-        />
-    )
+const style = {
+  paragraph: {
+    fontSize: '0.875rem',
+    lineHeight: '1.25rem',
+  },
 }
 
-function CustomCodeRenderer({data}: any) {
-    return (
-        <pre className='bg-gray-900 rounded-md p-4'>
-            <code className='text-gray-100'>{data.code}</code>
-        </pre>
-    )
-}
-
-function CustomImageRenderer({data}: any) {
-    const src = data.file.url
-
-    return (
-        <div className='relative w-full min-h-[15rem]'>
-            <Image
-                alt='image'
-                src={src}
-                fill
-                className='object-contain'
-            />
-        </div>
-    )
+const EditorOutput: FC<EditorOutputProps> = ({ content }) => {
+  return (
+    // @ts-ignore
+    <Output
+      style={style}
+      className='text-sm'
+      renderers={renderers}
+      data={content}
+    />
+  )
 }
 
 export default EditorOutput
